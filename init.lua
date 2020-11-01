d2b84fe410b1b1845732390ba232cce3cdf7fb83
James Greenleaf <james@greenle.af>
2020-11-01 12:19:28 -0500

    Add basic scrololo object

---
+++ init.lua
@@ -0,0 +1,9 @@
+local scrololo = {
+  name = "Scrololo",
+  version = "0.0.1",
+  author = "James Greenleaf",
+  license = "MIT",
+  homepage = "https://github.com/aMoniker/Scrololo.spoon"
+}
+
+return scrololo
